d871aa48b584cbb48d5a80cb71f712bd40d1bb94
黄鑫慧 <huangxinhui@wondersgroup.com>
2017-02-22 17:38:45 +0800

    up

--- testcounter/src/index.js
+++ testcounter/src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {createStore} from "redux";
 import Counter from "./components/Counter";
-im
+import counter from "./reducers";
 
 const store=createStore(couter);
 const rootEl=document.getElementById("root");
 
 const render=()=>ReactDOM.render(
-        <Couter />,
+        <Couter 
+        value={store.getState()}
+        onIncrement={()=>store.dispatch({type:'INCREMENT'})}
+        onDecrement={()=>store.dispatch({type:'DECREMENT'})}
+        />,
         rootEl   
     )
+
+    render();
+    store.subscribe(render)
